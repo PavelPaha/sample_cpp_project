847cc16165bc3ee106475c2beb193dc5b0cb12d5
pavelvasilev <vasilievpavel4@gmail.com>
2024-09-16 09:57:58 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include "my_add.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3){
+        std::cerr << "Provide exactly 2 arguments";
+        return 1;
+    }
+
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+
+    std::cout<<add(a, b)<<'\n';
+    return 0;
+}
